1fce21ef201ad4a4cb2842e18ab21545bb90aefa
Bradyn Glines <glinesbdev@gmail.com>
2018-01-03 21:09:58 -0700

    Initial Commit
    Added a login page component and set it as the root page

---
+++ src/pages/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+@Component({
+  templateUrl: 'login.html'
+})
+export class LoginPage implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
